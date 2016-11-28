088895527ff5ac350ede8e6ba449b0cbbb9b4697
Marco Montanari <sirmmo@gmail.com>
2016-11-28 16:10:49 +0100

    custom endpoints for minio integration

--- s3direct/utils.py
+++ s3direct/utils.py
@@ -67,7 +67,8 @@ def create_upload_data(content_type, key, acl, bucket=None, cache_control=None,
     secret_access_key = settings.AWS_SECRET_ACCESS_KEY
     bucket = bucket or settings.AWS_STORAGE_BUCKET_NAME
     region = getattr(settings, 'S3DIRECT_REGION', None)
-    endpoint = REGIONS.get(region, 's3.amazonaws.com')
+    custom_endpoint = getattr(settings, 'S3DIRECT_ENDPOINT', None)
+    endpoint = cusomt_endpoint or REGIONS.get(region, 's3.amazonaws.com')
 
     expires_in = datetime.utcnow() + timedelta(seconds=60*5)
     expires = expires_in.strftime('%Y-%m-%dT%H:%M:%S.000Z')
